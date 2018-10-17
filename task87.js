b6d6af351e413ebbaf4a4847867a78caa654555a
Vova <vovapankiw@gmail.com>
2018-10-17 11:47:30 +0300

    first commit

--- task87.js
+++ task87.js
@@ -1,5 +1,5 @@
 function sumOfDigits(n, m) {
-  var sum = 0;
+  let sum = 0;
   const digitArr = n.toString().split('');
   for (let i = 0; i < m; i += 1) {
     sum += parseInt(digitArr.pop(), 10);
